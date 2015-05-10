a78585da6ec32150aed23b701ec56e886a207e5a
wh1093109844 <1093109844@qq.com>
2015-05-10 18:41:56 +0800

    add a listener mothed

---
+++ MiuscPlayer/src/com/example/miuscplayer/PlayerView.java
@@ -0,0 +1,16 @@
+package com.example.miuscplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by hero on 2015/5/10.
+ */
+public class PlayerView extends RelativeLayout {
+
+    public PlayerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
--- MiuscPlayer/src/com/example/miuscplayer/aidl/OnProgressChanageListener.aidl
+++ MiuscPlayer/src/com/example/miuscplayer/aidl/OnProgressChanageListener.aidl
@@ -1,4 +1,5 @@
 package com.example.miuscplayer.aidl;
 interface OnProgressChanageListener{
 	void onProgressChange(in int progress, in int max);
+	void onBufferUpdateChange(in int buffer);
 }
